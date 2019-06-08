35d388f9ef134ea2e3d403977af57cc82043202f
verma888 <sevtagourav@gmail.com>
2019-06-08 07:05:26 +0530

    test files

---
+++ addnewpara.js
@@ -0,0 +1,9 @@
+var Student = {
+    Name : "Narendra Modi",
+    Age : "25",
+    Designation : "Web developer"
+
+}
+console.log(Student);
+Student.Address = "Bangalore, Karnataka";
+console.log(Student);
---
+++ deletepara.js
@@ -0,0 +1,11 @@
+var userDetails = {
+ 
+ Name : "Pramod Mahajan",
+ Occupation : "Engineer",
+ Organisation : "I-one tech Solution",
+ Address : "HSR layout, Bangalore, Karnataka",
+ ZipCode : "Not available"
+}
+console.log (userDetails);
+delete userDetails.ZipCode;
+console.log("\nAfter deleting zipcode the details are", '\n', userDetails);
